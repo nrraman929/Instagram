d686bbd9b9ed6c3559b6317c2f1b98decfde56cb
Nishant Raman <Nishant@wireless-10-146-219-117.public.utexas.edu>
2016-01-30 16:21:57 -0600

    prof pic

--- Instagram/PhotosViewController.swift
+++ Instagram/PhotosViewController.swift
@@ -55,7 +55,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("PhotoCell", forIndexPath: indexPath) as! PhotosViewCell
         
-        let movie = pictures![indexPath.row]
+        let movie = pictures![indexPath.section]
         let image = (movie["images"] as! NSDictionary)["standard_resolution"] as! NSDictionary
         let url = NSURL(string: image["url"] as! String)
         
@@ -65,11 +65,43 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         if let pic = pictures {
             return pic.count
         }
         return 0
     }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
+        
+        let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
+        profileView.clipsToBounds = true
+        profileView.layer.cornerRadius = 15;
+        profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).CGColor
+        profileView.layer.borderWidth = 1;
+        
+        // Use the section number to get the right URL
+        let movie = pictures![section]
+        let image = (movie["user"] as! NSDictionary)["profile_picture"] as! String
+        let url = NSURL(string : image)
+        profileView.setImageWithURL(url!)
+        
+        headerView.addSubview(profileView)
+        
+        // Add a UILabel for the username here
+        
+        return headerView
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
     
 }
 
